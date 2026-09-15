368028df60e800c8d4c83df91b5117c6a87c8e75
Epic Gamer <mamarco850@gmail.com>
2026-09-15 14:21:36 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -10,27 +10,29 @@ class FizzBuzz {
         for (int i = 1; i <= 100; i++) {
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            extracted(i);
         }
+    }
+
+    private static void extracted(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        String output = "";
+
+        // Print our appropriate result.
+        if (divisibleBy3) {
+            output += "Fizz";
+        }
+
+        if (divisibleBy5) {
+            output += "Buzz";
+        }
+
+        if (!divisibleBy3 && !divisibleBy5) {
+            output += Integer.toString(i);
+        }
+
+        System.out.println(output);
     }
 }
